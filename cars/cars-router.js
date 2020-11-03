2baf6c9ac7bdd3836cc2339ac5b9eed41ba2db63
sofia Luna <Sofia21luna@gmail.com>
2020-11-03 17:07:38 -0600

    gets cars by id and posts new cars

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -13,4 +13,29 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    db('cars').where({ id }).first()
+    .then(car => {
+        res.status(200).json(car)
+    })
+    .catch(error => {
+        res.status(500).json({error: error.message})
+    })
+})
+
+router.post('/', (req, res) => {
+    const carData = req.body
+    db('cars').insert(carData)
+    .then(ids => {
+        db('cars').where({id: ids[0]})
+        .then(newCar => {
+            res.status(201).json(newCar)
+        });
+    })
+    .catch(error => {
+        res.status(500).json({message: error.message})
+    })
+})
+
 module.exports = router;
