a3f4027b40feaa3f60506ef8efb454e4efbef76b
sofia Luna <Sofia21luna@gmail.com>
2020-11-03 18:00:17 -0600

    can update and delete cars

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -38,4 +38,33 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const updateCar = req.body
+
+    db('cars').where({ id }).update(updateCar)
+    .then((car) => {
+        if(car) {
+            res.status(200).json(car)
+        } else {
+            res.status(404).json({message: 'id not found'})
+        }
+    })
+    .catch(error => {
+        res.status(500).res.json({ error: error.message }) 
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db('cars').where({id}).del()
+    .then(() => {
+        res.status(200).json({message: 'account has been deleted'})
+    })
+    .catch(error => {
+        res.status(500).json({message: error.message})
+    })
+})
+
+
 module.exports = router;
